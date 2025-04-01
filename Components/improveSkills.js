ad227b125bd86caffff8e29e297b9ecafc9fa093
AgustinaLorda <aguslordat@gmail.com>
2025-04-01 14:37:59 -0400

    Update improveSkills.js

--- Components/improveSkills.js
+++ Components/improveSkills.js
@@ -1,24 +1,27 @@
-export default function ImproveSkills(){
-    const list = [
-        "Learn new recepies",
-        "Experiment with food",
-        "Explore Special Diets",
-        "Know nutrition facts",
-        "Get cooking tips",
-        "Share your recepies"
-    ]
-    return(
-         <div className="section improve-skills">
-            <div className="col img">
-                    <img src="/img/gallery/img_10.jpg" alt="" />
-                    </div>
-                    <div className="col typography"> 
-                        <h1 className="title">Improve Your Culinary Skills</h1>
-                        {list.map((item, index) => (
-                            <p className="skill-item" key={index}>{item}</p>
-                        ))}
-                        <button className="btn">signup now</button>
-                    </div>
-                </div>
-    )
+import { Link } from "react-router-dom";
+
+export default function ImproveSkills() {
+  const list = [
+    "Learn new recipes",
+    "Experiment with food",
+    "Explore Special Diets",
+    "Know nutrition facts",
+    "Get cooking tips",
+    "Share your recipes"
+  ];
+
+  return (
+    <div className="section improve-skills">
+      <div className="col img">
+        <img src="/img/gallery/img_10.jpg" alt="" />
+      </div>
+      <div className="col typography">
+        <h1 className="title">Improve Your Culinary Skills</h1>
+        {list.map((item, index) => (
+          <p className="skill-item" key={index}>{item}</p>
+        ))}
+        <Link to="/signup" className="btn signup-btn">Sign Up Now</Link>
+      </div>
+    </div>
+  );
 }
