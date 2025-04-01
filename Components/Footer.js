e861eda868d1e40e432cf2ee3b50daeb4ce4b77e
AgustinaLorda <aguslordat@gmail.com>
2025-04-01 12:42:18 -0400

    Create Footer.js

---
+++ Components/Footer.js
@@ -0,0 +1,24 @@
+export default function Footer(){
+    return(
+        <div className="footer container">
+             <div className="footer-section">
+                <p className="title">InstaRecipe.com</p>
+                <p>Transform ingredients into inspiration. Reduce waste, discover recipes, and share your culinary creations—all in one place. Cook smarter, connect deeper.</p>
+                <p>&copy; 2025 | All Rights Reserved  </p>
+             </div>
+             <div className="footer-section">
+                <p className="title">Contact Us</p>
+                <p>instarecipe@gmail.com</p>
+                <p>+1 484 734 2490</p>
+                <p>1570 Baltimore Pike</p>
+             </div>
+             <div className="footer-section">
+                <p className="title">Socials</p>
+                <p>Facebook</p>
+                <p>Twitter</p>
+                <p>Instagram</p>
+             </div>
+        </div>
+       
+    )
+}
